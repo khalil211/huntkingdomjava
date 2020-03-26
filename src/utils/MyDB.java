62622efecc904a089683e09c2de45a3b8101fc1a
khalil <khalil@192.168.1.5>
2020-03-26 20:27:31 +0100

    firt commit

---
+++ src/utils/MyDB.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyDB {
+    private Connection cnx;
+    private final String URL="jdbc:mysql://localhost:3306/huntkingdom";
+    private final String USERNAME="root";
+    private final String PASSWORD="";
+    
+    private static MyDB instance=null;
+    
+    private MyDB() {
+        try {
+            cnx=DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException ex) {
+            Logger.getLogger(MyDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static MyDB getInstance() {
+        if (instance==null)
+            instance=new MyDB();
+        return instance;
+    }
+    
+    public Connection getConnection() {
+        return cnx;
+    }
+}

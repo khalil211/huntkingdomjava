3ac0b1bc535d3b642622071027168d84e18a70dc
Moez6060 <elmoez.amira@esprit.tn>
2020-04-07 15:55:44 +0100

    Login/register/current user

--- src/huntkingdom/HuntKingdom.java
+++ src/huntkingdom/HuntKingdom.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import entities.user.CurrentUser;
 
 public class HuntKingdom extends Application {
     
@@ -12,9 +13,12 @@ public class HuntKingdom extends Application {
     
     @Override
     public void start(Stage s) throws Exception {
+        //CurrentUser cu= new CurrentUser(0);
+        
         stage=s;
         stage.setTitle("Hunt Kingdom");
         Parent root = FXMLLoader.load(getClass().getResource("/gui/MenuAdmin.fxml"));
+        //Parent root = FXMLLoader.load(getClass().getResource("/gui/login/Login.fxml"));
         Scene scene = new Scene(root, 1100, 600);
         stage.setScene(scene);
         stage.show();

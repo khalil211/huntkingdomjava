0a3c2394167ef7f4fb3f56a0a750794a9abbc438
louay122 <louay.gourrida@esprit.tn>
2020-04-22 11:18:27 +0100

    updated my work + some of user files

--- src/utils/MyDB.java
+++ src/utils/MyDB.java
@@ -8,10 +8,10 @@ import java.util.logging.Logger;
 
 public class MyDB {
     private Connection cnx;
-    //private final String URL="jdbc:mysql://127.0.0.1:3306/huntkingdom";
-    private final String URL="jdbc:mysql://127.0.0.1:8889/huntkingdom";
+    private final String URL="jdbc:mysql://127.0.0.1:3306/huntkingdom";
+    //private final String URL="jdbc:mysql://127.0.0.1:8889/huntkingdom";
     private final String USERNAME="root";
-    private final String PASSWORD="root";
+    private final String PASSWORD="";
     
     private static MyDB instance=null;
     
